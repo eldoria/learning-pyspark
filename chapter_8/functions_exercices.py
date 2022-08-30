dc1111b3004874ef1113407fb8a107c69a71c524
Maxime PEREZ <54.maxime.perez@gmail.com>
2022-08-30 10:57:12 +0200

    advance in part 8

--- chapter_8/functions_exercices.py
+++ chapter_8/functions_exercices.py
@@ -1,4 +1,5 @@
-from setup import sc
+from pyspark.sql import SparkSession
+from operator import add
 
 
 '''
@@ -6,6 +7,30 @@ The PySpark RDD API provides a count() method that returns the number of element
 Reproduce the behavior of this method using map(), filter(), and/or reduce().
 '''
 def ex_eight_one():
-    ex_rdd = sc.parallelize([1, "two", 3.0, ("four", 4), {"five": 5}])
+    spark = SparkSession.builder.getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    sc = spark.sparkContext
 
-    print(ex_rdd.reduce(lambda x: x+1))
+    collection_rdd = sc.parallelize([1, "two", 3.0, ("four", 4), {"five": 5}])
+
+    collection_rdd = collection_rdd.map(lambda _: 1).reduce(add)
+    print(collection_rdd)
+
+
+'''
+ex 8.2:
+
+What is the return value of the following code block?
+
+a_rdd = sc.parallelize([0, 1, None, [], 0.0])
+a_rdd.filter(lambda x: x).collect()
+
+a [1]
+b [0, 1]
+c [0, 1, 0.0]
+d []
+e [1, []]
+
+->
+a because in python empty sequences and 0/0.0 are considered false
+'''
--- chapter_8/lesson.py
+++ chapter_8/lesson.py
@@ -1,6 +1,5 @@
 from setup import spark
 
-
 collection = [1, "two", 3.0, ("four", 4), {"five": 5}]
 sc = spark.sparkContext
 collection_rdd = sc.parallelize(collection)
@@ -12,16 +11,96 @@ def safer_add_one(value):
     except TypeError:
         return value
 
+
 collection_rdd = collection_rdd.map(safer_add_one)
 print(collection_rdd.collect())
 
 collection_rdd = collection_rdd.filter(
- lambda elem: isinstance(elem, (float, int))
+    lambda elem: isinstance(elem, (float, int))
 )
 print(collection_rdd.collect())
 
-
 from operator import add
-collection_rdd = sc.parallelize([4, 7, 9, 1, 3])
-print(collection_rdd.reduce(add)) # 24
 
+collection_rdd = sc.parallelize([4, 7, 9, 1, 3])
+print(collection_rdd.reduce(add))  # 24
+
+################
+################
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+
+fractions = [[x, y] for x in range(100) for y in range(1, 100)]
+frac_df = spark.createDataFrame(fractions, ["numerator", "denominator"])
+frac_df = frac_df.select(
+    F.array(F.col("numerator"), F.col("denominator")).alias(
+        "fraction"
+    ),
+)
+frac_df.show(5, False)
+
+################
+################
+
+from fractions import Fraction
+from typing import Tuple, Optional
+
+Frac = Tuple[int, int]
+
+
+def py_reduce_fraction(frac: Frac) -> Optional[Frac]:
+    """Reduce a fraction represented as a 2-tuple of integers."""
+    num, denom = frac
+    if denom:
+        answer = Fraction(num, denom)
+        return answer.numerator, answer.denominator
+    return None
+
+
+assert py_reduce_fraction((3, 6)) == (1, 2)
+assert py_reduce_fraction((1, 0)) is None
+
+
+################
+################
+def py_fraction_to_float(frac: Frac) -> Optional[float]:
+    """Transforms a fraction represented as a 2-tuple of integers into a
+    float."""
+    num, denom = frac
+    if denom:
+        return num / denom
+    return None
+
+
+assert py_fraction_to_float((2, 8)) == 0.25
+assert py_fraction_to_float((10, 0)) is None
+
+################
+################
+
+SparkFrac = T.ArrayType(T.LongType())
+reduce_fraction = F.udf(py_reduce_fraction, SparkFrac)
+frac_df = frac_df.withColumn(
+    "reduced_fraction", reduce_fraction(F.col("fraction"))
+)
+frac_df.show(5, False)
+
+
+################
+################
+
+@F.udf(T.DoubleType())
+def fraction_to_float(frac: Frac) -> Optional[float]:
+    """Transforms a fraction represented as a 2-tuple of integers into a float."""
+    num, denom = frac
+    if denom:
+        return num / denom
+    return None
+
+
+frac_df = frac_df.withColumn(
+    "fraction_float", fraction_to_float(F.col("reduced_fraction"))
+)
+frac_df.select("reduced_fraction", "fraction_float").distinct().show(
+    5, False
+)
